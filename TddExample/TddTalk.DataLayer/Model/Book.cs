f3f31aa4affca5fdad9e6c92c9e381396239b343
alemartinezb <amartinez@revenuewell.com>
2023-05-08 19:21:48 -0300

    TDDTalk first approach

---
+++ TddExample/TddTalk.DataLayer/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddTalk.DataLayer.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Editorial { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public string Icbn { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}

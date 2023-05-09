bd03a326869e8e1c95e4e2109d81a5cc8193549e
alemartinezb <amartinez@revenuewell.com>
2023-05-09 13:26:59 -0300

    First chicago approach

---
+++ TddExample/TddTalk.DataLayer/Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TddTalk.DataLayer.Model;
+
+namespace TddTalk.DataLayer.Interfaces
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book>GetByAuthorId(int authorId);
+    }
+}
---
+++ TddExample/TddTalk.Test/BookRepositoryTest.cs
@@ -0,0 +1,51 @@
+using TddTalk.DataLayer.Context;
+using TddTalk.DataLayer.Model;
+using TddTalk.DataLayer.Repository;
+
+namespace TddTalk.Test
+{
+    [TestClass]
+    public class BookRepositoryTest
+    {
+        private BookDbContext _bookContext;
+        private BookRepository _bookRepository;
+        [TestInitialize]
+        public void Setup()
+        {
+            var filePath = "";
+            _bookRepository = new BookRepository(filePath);
+        }
+
+        [TestMethod]
+        public void BooksShouldHaveTitleISBNCreationDateAuthorIdAndId()
+        {
+            var books = _bookRepository.GetByAuthorId(1);
+
+            Assert.IsNotNull(books);
+            Assert.IsNotNull(books.FirstOrDefault().Id);
+            Assert.IsNotNull(books.FirstOrDefault().Title);
+            Assert.IsNotNull(books.FirstOrDefault().ISBN);
+            Assert.IsNotNull(books.FirstOrDefault().CreationDate);
+            Assert.IsNotNull(books.FirstOrDefault().AuthorId);
+        }
+        [TestMethod]
+        public void AuthorShouldHaveFirstNameLastNameAndId()
+        {
+            
+            var books = _bookRepository.GetByAuthorId(1);
+
+            Assert.IsNotNull(books);
+            Assert.IsNotNull(books.FirstOrDefault().Author.Id);
+            Assert.IsNotNull(books.FirstOrDefault().Author.FirstName);
+            Assert.IsNotNull(books.FirstOrDefault().Author.LastName);
+        }
+        [TestMethod]
+        public void ShouldGetAListOfBooksWithSomeElements()
+        {
+            var books = _bookRepository.GetByAuthorId(1);
+
+            Assert.IsNotNull(books);
+            Assert.AreNotSame(0, books.Count());
+        }
+    }
+}

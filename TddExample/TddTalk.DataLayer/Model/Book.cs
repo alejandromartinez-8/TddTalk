5029a15c12a0cca044710e374e800afcffae573c
alemartinezb <amartinez@revenuewell.com>
2023-05-08 21:31:04 -0300

    Add files with data

--- TddExample/TddTalk.DataLayer/Model/Book.cs
+++ TddExample/TddTalk.DataLayer/Model/Book.cs
@@ -10,12 +10,10 @@ namespace TddTalk.DataLayer.Model
     public class Book
     {
         public int Id { get; set; }
-        public string Name { get; set; }
         public string Title { get; set; }
-        public string Editorial { get; set; }
         public int AuthorId { get; set; }
         public Author Author { get; set; }
-        public string Icbn { get; set; }
+        public string ISBN { get; set; }
         public DateTime CreationDate { get; set; }
     }
 }

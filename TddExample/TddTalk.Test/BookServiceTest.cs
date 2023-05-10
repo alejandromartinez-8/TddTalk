b474811e88f349d211b62011d0b6c6eaef44b381
alemartinezb <amartinez@revenuewell.com>
2023-05-10 18:26:22 -0300

    Revert "Delete code"
    This reverts commit 5bbbf4ed39c39afb74ae2cfd328e0c622ad462c0.

--- TddExample/TddTalk.Test/BookServiceTest.cs
+++ TddExample/TddTalk.Test/BookServiceTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TddExample.Services.Model;
+using TddExample.Services.Services;
 using TddTalk.DataLayer.Interfaces;
 using TddTalk.DataLayer.Model;
 using TddTalk.DataLayer.Repository;

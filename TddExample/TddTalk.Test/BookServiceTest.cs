232b567cfe7e83d99bb40809680483b9d8134ceb
alemartinezb <amartinez@revenuewell.com>
2023-05-10 16:18:35 -0300

    First step of tests

--- TddExample/TddTalk.Test/BookServiceTest.cs
+++ TddExample/TddTalk.Test/BookServiceTest.cs
@@ -6,8 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 using TddExample.Services.Interfaces;
 using TddExample.Services.Model;
-using TddExample.Services.Services;
-using TddTalk.DataLayer.Interfaces;
 using TddTalk.DataLayer.Model;
 
 namespace TddTalk.Test
@@ -15,95 +13,21 @@ namespace TddTalk.Test
     [TestClass]
     public class BookServiceTest
     {
-        private BookService _bookService;
-        private Mock<IBookRepository> _bookRepository;
+
         [TestInitialize] public void SetUp() 
         {
-            _bookRepository = new Mock<IBookRepository>();
-            _bookService = new BookService(_bookRepository.Object);
+
         }
         [TestMethod]
         public void Should_GetAListOfBooks_When_AuthorExists()
         {
-            var authorId = 2;
-            var books = new List<Book>()
-            {
-                new Book
-                { 
-                    Id = 5,
-                    Title="Growing Object-Oriented Software Guided by Tests",
-                    AuthorId=2,
-                    ISBN="9780321503626",
-                    CreationDate=new DateTime(2009,01,01),
-                    Author = new Author
-                    {
-                        Id=2,
-                        FirstName="Nat",
-                        LastName="Price"
-                    }
-                }
-            };
-            _bookRepository.Setup(b => b.GetByAuthorId(authorId)).Returns(books);
-
-            var mockBooks = new List<BookSearchDto>()
-            {
-                new BookSearchDto
-                    {
-                    Id = 5,
-                    Title="Growing Object-Oriented Software Guided by Tests",
-                    AuthorId=2,
-                    ISBN="9780321503626",
-                    CreationDate=new DateTime(2009,01,01),
-                }
-            };
-
-            var result = _bookService.GetByAuthorId(authorId);
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOfType(result, typeof(List<BookSearchDto>));
+            
         }
 
         [TestMethod]
         public void Should_GetTheListWithBooks_When_AuthorIdIsEqualTo2()
         {
-            var authorId = 2;
-            var books = new List<Book>()
-            {
-                new Book
-                {
-                    Id = 5,
-                    Title="Growing Object-Oriented Software Guided by Tests",
-                    AuthorId=2,
-                    ISBN="9780321503626",
-                    CreationDate=new DateTime(2009,01,01),
-                    Author = new Author
-                    {
-                        Id=2,
-                        FirstName="Nat",
-                        LastName="Price"
-                    }
-                }
-            };
-            _bookRepository.Setup(b => b.GetByAuthorId(authorId)).Returns(books);
-
-            var mockBooks = new List<BookSearchDto>()
-            {
-                new BookSearchDto
-                    {
-                    Id = 5,
-                    Title="Growing Object-Oriented Software Guided by Tests",
-                    AuthorId=2,
-                    ISBN="9780321503626",
-                    CreationDate=new DateTime(2009,01,01),
-                }
-            };
-
-            var result = _bookService.GetByAuthorId(authorId);
-
-            Assert.IsNotNull(result);
-            Assert.AreEqual(mockBooks.FirstOrDefault().ISBN, result.FirstOrDefault().ISBN);
-            Assert.AreEqual(mockBooks.FirstOrDefault().Id, result.FirstOrDefault().Id);
-            Assert.AreEqual(mockBooks.FirstOrDefault().Title, result.FirstOrDefault().Title);
-
+           
         }
     }
 }

770e4ebeed6a449fb03a18ffcc13b21d13725903
alemartinezb <amartinez@revenuewell.com>
2023-05-10 18:33:40 -0300

    Clean up code

--- TddExample/TddTalk.Test/BookControllerTest.cs
+++ TddExample/TddTalk.Test/BookControllerTest.cs
@@ -13,40 +13,28 @@ namespace TddTalk.Test
     [TestClass]
     public class BookControllerTest
     {
-        private BookController _bookController;
+
         [TestInitialize] public void SetUp() 
         {
-            var bookRepository = new BookRepository("");
-            var bookService = new BookService(bookRepository);
-            _bookController = new BookController(bookService);
+
         }
 
         [TestMethod]
         public void Should_GetAListOfBooks_When_AnAuthorHasBooks()
         {
-            var books = _bookController.GetByAuthorId(1);
 
-            Assert.IsNotNull(books);
-            Assert.AreEqual(1,books.FirstOrDefault().AuthorId);
         }
 
         [TestMethod]
         public void Should_GetAnEmptyList_When_AuthorDoesNotExist()
         {
-            var books = _bookController.GetByAuthorId(4);
-
-            Assert.IsNotNull(books);
-            Assert.AreEqual(0, books.Count());
 
         }
 
         [TestMethod]
         public void Should_GetAnEmptyList_When_AuthorDoesNotHaveAnyBook()
         {
-            var books = _bookController.GetByAuthorId(3);
 
-            Assert.IsNotNull(books);
-            Assert.AreEqual(0, books.Count());
         }
     }
 }

f5fc5e3123a00c5f9929f6aac9a706122c101ae8
alemartinezb <amartinez@revenuewell.com>
2023-05-10 17:06:31 -0300

    Fix merge problems

--- TddExample/TddTalk.Test/BookServiceTest.cs
+++ TddExample/TddTalk.Test/BookServiceTest.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using TddExample.Services.Interfaces;
 using TddExample.Services.Model;
+using TddExample.Services.Services;
+using TddTalk.DataLayer.Interfaces;
 using TddTalk.DataLayer.Model;
 
 namespace TddTalk.Test
